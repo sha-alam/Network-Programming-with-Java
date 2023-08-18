f278cb38beae39166527713c932cc0afd6a2e8aa
Md Sha Alam <shaalam.ice@pust.ac.bd>
2023-08-18 20:30:22 +0600

    First Submit

---
+++ java_sql_db_delete_data.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class java_sql_db_delete_data {
+	public static void main(String[] args) {
+		try {
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");// Loading Driver
+			Connection connection = DriverManager.getConnection(
+					"jdbc:ucanaccess://E:\\Course\\3-2\\Network Programming with Java\\Lab\\Solution\\Database\\Lab_18.accdb");// Establishing
+																																// Connection
+			System.out.println("Connected Successfully");
+			// Crating PreparedStatement object
+			PreparedStatement preparedStatement = connection.prepareStatement("delete from Practice  where Phone=?");
+			// Setting values for Each Parameter
+			preparedStatement.setString(1, "01722633140");
+			preparedStatement.executeUpdate();
+			System.out.println("data Delete successfully");
+
+		} catch (Exception e) {
+			System.out.println("Error in connection");
+
+		}
+
+	}
+}
